ad13478a387d575ffe0aee7cd2bebc0195835172
Daniel <daniel.lien@phantom.ai>
2025-02-27 19:58:02 -0800

    aggregate data eda, questionId splitting

--- workspaces/daniel/copd_data_cleaning.py
+++ workspaces/daniel/copd_data_cleaning.py
@@ -2,35 +2,51 @@
 Dataset Cleaning for Chronic Obstructive Pulmonary Disease (COPD)
 """
 
-import pandas as pd
 import pdb
+import pandas as pd
 from pathlib import Path
 
 #input your relative path here 
 rel_path = Path("/home/daniel.lien/dev/homework/data/")
-dataset_csv = Path("U.S._Chronic_Disease_Indicators_20250204.csv")
+dataset_csv = Path("homework/data/U.S._Chronic_Disease_Indicators__CDI___2023_Release copy.csv") # Start with Main Dataset 
 
 
-## General Cleaning
+### General Cleaning
 # Step 1: Read in Data
 df = pd.read_csv(rel_path / dataset_csv)
 
 # Step 2: Filter The Data for COPD-related records
 df = df[df['Topic'] == 'Chronic Obstructive Pulmonary Disease']
 
-# Step 3: Drop Uncessesary Columns - 
-columns_to_drop = ['Response','ResponseID',
-            'Stratification2','StratificationCategory2','StratificationID2','StratificationCategoryID2',
-            'Stratification3', 'StratificationCategory3', 'StratificationID3','StratificationCategoryID3',
-            'DataValueAlt', 'DataValueFootnoteSymbol', 'DataValueFootnote','LocationID', 'LocationAbbr',
-            'TopicID', 'DataValueTypeID', 'DataSource', 'Topic', 'LowConfidenceLimit', 'HighConfidenceLimit', 'YearEnd']
+columns_to_drop = ['Response', 
+            'StratificationCategory2',
+            'Stratification2',
+            'StratificationCategory3',
+            'Stratification3',
+            'ResponseID',
+            'StratificationCategoryID2', 
+            'StratificationID2',         
+            'StratificationCategoryID3',
+            'StratificationID3',
+            'YearEnd',
+            'LocationAbbr',
+            'DataSource',
+            'DataValueAlt',
+            'DataValueFootnoteSymbol',
+            'DataValueFootnote',
+            'HighConfidenceLimit',
+            'LowConfidenceLimit',
+            'LocationID',
+            'TopicID',
+            'DataValueTypeID'
+    ]
 df = df.drop(columns=columns_to_drop)
 
 
 ## Specific Cleaning
 """TODO
 Remove non states from LocationDes c = ["Virgin Islands", "Puerto Rico", "Guam", "District of Columbia","United States"]
-Handle missing values in DataValue ~30%
+Handle missing values in DataValue ~30% --? All Removed 
 """
 # Step 4: Identify and handle missing values 
 # ~30% of data is missing in the 'DataValue' column, so let's remove those rows
@@ -47,40 +63,40 @@ states_to_keep = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Col
 df = df[df['LocationDesc'].isin(states_to_keep)]
 
 
-# Step 5: Group into 6 dataframes based on "QuestionID"
-questions = df['Question'].unique()
-df_grouped = {}
+# # Step 5: Group into 6 dataframes based on "QuestionID"
+# questions = df['Question'].unique()
+# df_grouped = {}
 
-for question in questions:
-    df_grouped[question] = df[df['Question'] == question].reset_index(drop=True)
+# for question in questions:
+#     df_grouped[question] = df[df['Question'] == question].reset_index(drop=True)
 
-# View head of each dataframe formatted nicely 
-for question, group in df_grouped.items():
-    print(f"Question: {question}")
-    print(group.head()) 
+# # View head of each dataframe formatted nicely 
+# for question, group in df_grouped.items():
+#     print(f"Question: {question}")
+#     print(group.head()) 
 
-# Step 6: Reset Index for each dataframe
-for question, group in df_grouped.items():
-    df_grouped[question].reset_index(drop=True, inplace=True)
+# # Step 6: Reset Index for each dataframe
+# for question, group in df_grouped.items():
+#     df_grouped[question].reset_index(drop=True, inplace=True)
 
 
-QUESTION_MAP = {
-    "Chronic obstructive pulmonary disease among adults" : "COPD01",
-    "Current smoking among adults with chronic obstructive pulmonary disease" : "COPD02",
-    "Hospitalization for chronic obstructive pulmonary disease as any diagnosis, Medicare-beneficiaries aged 65 years and older" : "COPD03",
-    "Hospitalization for chronic obstructive pulmonary disease as principal diagnosis, Medicare-beneficiaries aged 65 years and older" : "COPD04",
-    "Chronic obstructive pulmonary disease mortality among adults aged 45 years and older, underlying cause" : "COPD05",
-    "Chronic obstructive pulmonary disease mortality among adults aged 45 years and older, underlying or contributing cause" : "COPD06"
+# QUESTION_MAP = {
+#     "Chronic obstructive pulmonary disease among adults" : "COPD01",
+#     "Current smoking among adults with chronic obstructive pulmonary disease" : "COPD02",
+#     "Hospitalization for chronic obstructive pulmonary disease as any diagnosis, Medicare-beneficiaries aged 65 years and older" : "COPD03",
+#     "Hospitalization for chronic obstructive pulmonary disease as principal diagnosis, Medicare-beneficiaries aged 65 years and older" : "COPD04",
+#     "Chronic obstructive pulmonary disease mortality among adults aged 45 years and older, underlying cause" : "COPD05",
+#     "Chronic obstructive pulmonary disease mortality among adults aged 45 years and older, underlying or contributing cause" : "COPD06"
 
-}
-# Step 7: Save the cleaned dataset by question number
-for i, (question, group) in enumerate(df_grouped.items(), 1):
-    question_id = QUESTION_MAP[question]
-    file_name = f"/home/daniel.lien/dev/homework/data/cleaned_copd_data_question_{question_id}.csv"
-    group.to_csv(file_name, index=False)
-    print(f"Saved cleaned data for question {i} as {file_name}")
+# }
+# # Step 7: Save the cleaned dataset by question number
+# for i, (question, group) in enumerate(df_grouped.items(), 1):
+#     question_id = QUESTION_MAP[question]
+#     file_name = f"/home/daniel.lien/dev/homework/data/cleaned_copd_data_question_{question_id}.csv"
+#     group.to_csv(file_name, index=False)
+#     print(f"Saved cleaned data for question {i} as {file_name}")
 
-print("Data cleaning complete. Cleaned datasets saved as 'cleaned_copd_data_question_X.csv'.")
+# print("Data cleaning complete. Cleaned datasets saved as 'cleaned_copd_data_question_X.csv'.")
 
 
 
---
+++ workspaces/daniel/readme.md
@@ -0,0 +1,91 @@
+Questions:
+
+1) COPD disease among adults (Q01)
+Response is all Percentages (prevalence)
+2) Current smoking adults with COPD  (Q02)
+Response is all Percentages (prevalence)
+3) Hospitalization for COPD as any diagnosis, medicare aged 65+ (Q03)
+Cases per 1000, number (number, AA rate, crude rate)
+4) Hospitalization for COPD as principal diagnosis, medicare aged 65+ (Q04)
+Cases per 1000, number (number, AA rate, crude rate)
+5) COPD mortality as underlying cause 45+ (Q05)
+Cases per 100,000, number (number, AA rate, crude rate)
+6) COPD mortality as underlying or contributing cause 45+ (Q06)
+Cases per 100,000, number (number, AA rate, crude rate)
+
+
+
+# In Order 
+- Chronic obstructive pulmonary disease among adults'
+- Current smoking among adults with chronic obstructive pulmonary disease',
+- Hospitalization for chronic obstructive pulmonary disease as any diagnosis, Medicare-beneficiaries aged 65 years and older'
+- Hospitalization for chronic obstructive pulmonary disease as principal diagnosis, Medicare-beneficiaries aged 65 years and older'
+- 'Chronic obstructive pulmonary disease mortality among adults aged 45 years and older, underlying cause',
+-'Chronic obstructive pulmonary disease mortality among adults aged 45 years and older, underlying or contributing cause',
+
+Thoughts:
+
+Combine Hospitalization Rates: Any Diagnosis 
+Combine Mortality Rates : underlying and contributing cause
+Current COPD 
+Current Smoking
+
+
+My Map will have a year selection (for location/year)
+after the year is selected,
+I will have 4 Options to select
+- hopitalization rate of aged 65 older
+- COPD Disease
+- Smoking Prevalence
+- Mortality Rate 
+
+- Mortality - underlying or contributing
+
+- Hospitalization - first listed diag, any diag, first listed > 65, any diag > 65 
+
+- ED - first listed, any diag
+
+- Prevalence COPd - >= 18, >= 45 
+
+- Prevalence Smoking - >=18, >=45
+
+- Prevalance of cur adult > 18 with copd
+
+- Prevalence of cur adult > 45 with copd
+
+- pneumococcal vaccine among NI adults with >= 45 years
+
+- 
+
+
+['Mortality with chronic obstructive pulmonary disease as underlying cause among adults aged >= 45 years',
+
+'Mortality with chronic obstructive pulmonary disease as underlying or contributing cause among adults aged >= 45 years',
+
+'Hospitalization for chronic obstructive pulmonary disease as first-listed diagnosis',
+
+'Hospitalization for chronic obstructive pulmonary disease as any diagnosis',
+
+'Hospitalization for chronic obstructive pulmonary disease as first-listed diagnosis among Medicare-eligible persons aged >= 65 years',
+
+'Hospitalization for chronic obstructive pulmonary disease as any diagnosis among Medicare-eligible persons aged >= 65 years',
+
+'Emergency department visit rate for chronic obstructive pulmonary disease as first-listed diagnosis',
+
+'Emergency department visit rate for chronic obstructive pulmonary disease as any diagnosis',
+
+'Prevalence of chronic obstructive pulmonary disease among adults >= 18',
+
+'Prevalence of chronic obstructive pulmonary disease among adults >= 45 years',
+
+'Prevalence of current smoking among adults >= 18 with diagnosed chronic obstructive pulmonary disease',
+
+'Prevalence of current smoking among adults >= 45 years with diagnosed chronic obstructive pulmonary disease',
+
+'Pneumococcal vaccination among noninstitutionalized adults aged >= 45 years with chronic obstructive pulmonary disease',
+
+'Prevalence of activity limitation among adults >= 45 years with diagnosed chronic obstructive pulmonary disease',
+
+'Influenza vaccination among noninstitutionalized adults aged >= 45 years with chronic obstructive pulmonary disease',
+
+'Prevalence of activity limitation among adults >= 18 with diagnosed chronic obstructive pulmonary disease']
